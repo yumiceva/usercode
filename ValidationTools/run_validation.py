3332b52b7e51012f6874b50374cd30f1753ae957
yumiceva <>
2007-05-23 18:13:24 +0000

    stable version

--- ValidationTools/run_validation.py
+++ ValidationTools/run_validation.py
@@ -24,7 +24,7 @@
    -5, --sample5 = SAMPLE5: cfg sample
    -6, --sample6 = SAMPLE6: cfg sample
    -r, --reference = REFERENCE: CMSSW version of reference plots
-   -n, --nocompare : do not compare histograms only produce plots. It can be used to create reference plots
+   -n, --nocompare : do not compare histograms only produce plots. It can be used to create reference plots.
 """
 
 # Modules
@@ -38,8 +38,47 @@ import time
 import socket, string
 import getopt, popen2, fcntl, select, string, glob
 import tempfile
+import optparse
 
 #from ROOT import gROOT
+USAGE = re.compile(r'(?s)\s*usage: (.*?)(\n[ \t]*\n|$)')
+
+def nonzero(self): # will become the nonzero method of optparse.Values
+    "True if options were given"
+    for v in self.__dict__.itervalues():
+        if v is not None: return True
+    return False
+
+optparse.Values.__nonzero__ = nonzero # dynamically fix optparse.Values
+
+class ParsingError(Exception): pass
+
+optionstring=""
+
+def exit(msg=""):
+    raise SystemExit(msg or optionstring.replace("%prog",sys.argv[0]))
+
+def parse(docstring, arglist=None):
+    global optionstring
+    optionstring = docstring
+    match = USAGE.search(optionstring)
+    if not match: raise ParsingError("Cannot find the option string")
+    optlines = match.group(1).splitlines()
+    try:
+        p = optparse.OptionParser(optlines[0])
+        for line in optlines[1:]:
+            opt, help=line.split(':')[:2]
+            short,long=opt.split(',')[:2]
+            if '=' in opt:
+                action='store'
+                long=long.split('=')[0]
+            else:
+                action='store_true'
+            p.add_option(short.strip(),long.strip(),
+                         action = action, help = help.strip())
+    except (IndexError,ValueError):
+        raise ParsingError("Cannot parse the option string correctly")
+    return p.parse_args(arglist)
 
 
 #__________________
@@ -154,9 +193,9 @@ def get_list_files_ls(prefix,check = ""):
 #________________________________________________________________
 if __name__ == "__main__":
 
-    import optionparse
-    option,args = optionparse.parse(__doc__)
-    if not args and not option: optionparse.exit()
+    #import optionparse
+    option,args = parse(__doc__)
+    if not args and not option: exit()
 
     webpath = ""
     cmssw_reference = "CMSSW_1_3_1"
