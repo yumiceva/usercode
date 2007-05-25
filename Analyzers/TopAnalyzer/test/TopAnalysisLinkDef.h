f2653863231f270b6a363948e4941fa77482a77d
yumiceva <>
2007-05-25 22:08:44 +0000

    fix warnings

--- Analyzers/TopAnalyzer/test/TopAnalysisLinkDef.h
+++ Analyzers/TopAnalyzer/test/TopAnalysisLinkDef.h
@@ -1,8 +1,10 @@
 #include "Analyzers/TopAnalyzer/test/TopAnalysis.h"
 
+#ifdef __CINT__
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
 
 
 #pragma link C++ class TopAnalysis+;
+#endif
