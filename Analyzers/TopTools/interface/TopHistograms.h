9fcfa2f3f421e639cc05b650c51d49f2cf780e33
yumiceva <>
2007-05-26 05:03:33 +0000

    update of histograms

--- Analyzers/TopTools/interface/TopHistograms.h
+++ Analyzers/TopTools/interface/TopHistograms.h
@@ -8,7 +8,7 @@
 
  author: Francisco Yumiceva, Fermilab (yumiceva@fnal.gov)
 
- version $Id: TopHistograms.h,v 1.1 2007/05/25 19:47:13 yumiceva Exp $
+ version $Id: TopHistograms.h,v 1.2 2007/05/25 21:27:33 yumiceva Exp $
 
 ________________________________________________________________**/
 
@@ -29,7 +29,13 @@ class TopHistograms {
 	void fill1d(TString name, Double_t x );
 	void fill2d(TString name, Double_t x, Double_t y);
 	void Print(TString extension="png", TString tag="");
-	void SaveToFile(TString filename="plots_summary.root");
+	void SaveToFile(TString filename="Ttplots.root");
+
+  private:
+
+	std::map<std::string, TCanvas*> cv_map;
+	std::map<std::string, TH1*> h1;
+	std::map<std::string, TH2*> h2;
 	
 	
 }
