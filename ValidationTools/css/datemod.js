16effcd52ce34919d7370bb91ff40507ec38c6bb
yumiceva <>
2007-11-09 15:31:36 +0000

    organize package

--- ValidationTools/css/datemod.js
+++
@@ -1,9 +0,0 @@
-// Formatted last-modified date
-if (Date.parse(document.lastModified) != 0) {
-  var modiDate = new Date(document.lastModified);
-  var monthName = new Array("January", "February", "March", "April", "May", 
-   "June", "July", "August", "September", "October", "November", "December");
-  var dateGMT = modiDate.toLocaleString();
-  document.write("Last modified: " + monthName[modiDate.getMonth()] + " ");
-  document.write(modiDate.getDate() + ", " + modiDate.getFullYear() + " " + dateGMT.substring(dateGMT.indexOf(':')-2,dateGMT.indexOf(':')+6));
-  }
