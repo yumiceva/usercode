56ed105d3220be9ae7843493674cc84b8e3d91e5
yumiceva <>
2008-08-04 15:55:52 +0000

    put ROOT style in a separate class

--- cuy/scripts/Inspector.py
+++ cuy/scripts/Inspector.py
@@ -21,31 +21,13 @@ class Inspector:
     def SetFilename(self, value):
 	self.Filename = value
 
-    def Loop(self, value):
+    def Loop(self):
 		
 	alist = self.dir.GetListOfKets()
 
 	for i in alist:
 	    aobj = i.ReadObj()
 	    if obj.IsA().InheritsFrom("TDirectory"):
-		print 'y'
-    
-    def GetListObjects(self):
-	
-	afile = TFile(self.Filename)
-	
-	if afile.IsZombie():
-	    print " error trying to open file: " + self.Filename
-	    sys.exit()
-	
-	#alist = afile.GetListOfKeys()
-	self.dir = ROOT.gDirectory
-	
-	alist = self.dir.GetListOfKeys()
-
-	for i in alist:
-	    aobj = i.ReadObj()
-	    if aobj.IsA().InheritsFrom("TDirectory"):
 		print ' found directory: '+i.GetName()
 		
 		bdir = self.dir
@@ -55,8 +37,36 @@ class Inspector:
 		    bobj = j.ReadObj()
 		    if bobj.IsA().InheritsFrom(ROOT.TH1.Class()):
 			print '  --> found TH1: '+j.GetName()
+
+    def GetListObjects(self):
+	
+	afile = TFile(self.Filename)
+	
+	if afile.IsZombie():
+	    print " error trying to open file: " + self.Filename
+	    sys.exit()
+	
+	self.dir = ROOT.gDirectory
+	Inspector.Loop()
+
+#	self.dir = ROOT.gDirectory
+#	
+#	alist = self.dir.GetListOfKeys()
+#
+#	for i in alist:
+#	    aobj = i.ReadObj()
+#	    if aobj.IsA().InheritsFrom("TDirectory"):
+#		print ' found directory: '+i.GetName()
+#		
+#		bdir = self.dir
+#		afile.GetObject(i.GetName(),bdir)
+#		blist = bdir.GetListOfKeys()
+#		for j in blist:
+#		    bobj = j.ReadObj()
+#		    if bobj.IsA().InheritsFrom(ROOT.TH1.Class()):
+#			print '  --> found TH1: '+j.GetName()
     
-	#loop(dir)
+	
 
     
 
--- cuy/scripts/cuy.py
+++ cuy/scripts/cuy.py
@@ -35,7 +35,7 @@ import os, string, re, sys
 try:
     import ROOT
 except:
-    print "Cannot load PYROOT, make sure you have setup ROOT in the path"
+    print "\nCannot load PYROOT, make sure you have setup ROOT in the path"
     print "and pyroot library is also defined in the variable PYTHONPATH, try:\n"
     if (os.getenv("PYTHONPATH")):
 	print " setenv PYTHONPATH ${PYTHONPATH}:$ROOTSYS/lib\n"
@@ -53,6 +53,7 @@ from xml.sax import saxutils, make_parser, handler
 from xml.sax.handler import feature_namespaces
 
 import Inspector
+import Style
 
 #_______________OPTIONS________________
 import optparse
@@ -209,47 +210,8 @@ class FindIssue(handler.ContentHandler):
 if __name__ == '__main__':
 
     # style
-    ROOT.gStyle.SetFrameBorderMode(0)
-    ROOT.gStyle.SetCanvasBorderMode(0)
-    ROOT.gStyle.SetPadBorderMode(0)
-
-    ROOT.gStyle.SetPadColor(0)
-    ROOT.gStyle.SetCanvasColor(0)
-    ROOT.gStyle.SetTitleColor(1)
-    ROOT.gStyle.SetStatColor(0)
-
-    # set the paper & margin sizes
-    ROOT.gStyle.SetPaperSize(20,26)
-    ROOT.gStyle.SetPadTopMargin(0.04)
-    ROOT.gStyle.SetPadRightMargin(0.04)
-    ROOT.gStyle.SetPadBottomMargin(0.14)
-    ROOT.gStyle.SetPadLeftMargin(0.16)
-    ROOT.gStyle.SetPadTickX(1)
-    ROOT.gStyle.SetPadTickY(1)
-    
-    
-    ROOT.gStyle.SetTextFont(132)
-    ROOT.gStyle.SetTextSize(0.09)
-    ROOT.gStyle.SetLabelFont(132,"xyz")
-    ROOT.gStyle.SetTitleFont(132,"xyz")
-    ROOT.gStyle.SetLabelSize(0.035,"xyz")
-    ROOT.gStyle.SetTitleSize(0.045,"xyz")
-    ROOT.gStyle.SetTitleOffset(1.5,"y")
-    
-    # use bold lines and markers
-    ROOT.gStyle.SetMarkerStyle(8)
-    ROOT.gStyle.SetHistLineWidth(2)
-    ROOT.gStyle.SetLineWidth(1)
-    #ROOT.gStyle.SetLineStyleString(2,"[12 12]") // postscript dashes
-
-    # do not display any of the standard histogram decorations
-    ROOT.gStyle.SetOptTitle(0)
-    ROOT.gStyle.SetOptStat(0) #("m")
-    ROOT.gStyle.SetOptFit(0)
-    
-    #ROOT.gStyle.SetPalette(1,0)
-    ROOT.gStyle.cd()
-    ROOT.gROOT.ForceStyle()
+    thestyle = Style.Style()
+    thestyle.SetStyle()
 
     printCanvas = False
     printFormat = "png"
