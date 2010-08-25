5f5c34e33e3c267e7d9bec2507d180a5568779d9
yumiceva <>
2010-08-25 19:57:53 +0000

    update

---
+++ Top7TeV/test/tools/histograms.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python
+
+from ROOT import *
+
+class Hist:
+
+    def __init__(self):
+
+        self.muons = {}
+        self.electrons = {}
+        self.jets = {}
+        self.M3 = {}
+        self.M3chi2 = {}
+        self.all = {}
+
+    def Create(self, name):
+
+        self.CreateMuon(name)
+        self.all = self.muons + self.electrons + self.jets + self.M3 + self.M3chi2
+
+        for key in self.all.keys():
+            self.all[key].SetXTitle(self.all[key].GetTitle())
+
+    def Write(self):
+        for key in self.all.keys():
+            self.all[key].Write()
+    
+    def CreateMuon(self, name):
+        name = "_"+name
+        self.muons['pt'+name] = TH1F("muon_pt"+name,"p_{T}^{#mu}", 25, 0, 100)
+        self.muons['pt_1jet'+name] = TH1F("muon_pt_1jet"+name,"p_{T}^{#mu}", 25, 20, 100)
+        self.muons['pt_2jet'+name] = TH1F("muon_pt_2jet"+name,"p_{T}^{#mu}", 25, 20, 100)
+        self.muons['pt_3jet'+name] = TH1F("muon_pt_3jet"+name,"p_{T}^{#mu}", 25, 20, 100)
+        self.muons['pt_4jet'+name] = TH1F("muon_pt_4jet"+name,"p_{T}^{#mu}", 25, 20, 100)
+        self.muons['eta'+name] = TH1F("muon_eta"+name,"#eta^{#mu}", 20, -2.1, 2.1)
+        self.muons['eta_1jet'+name] = TH1F("muon_eta_1jet"+name,"#eta^{#mu}", 20, -2.1, 2.1)
+        self.muons['eta_2jet'+name] = TH1F("muon_eta_2jet"+name,"#eta^{#mu}", 20, -2.1, 2.1)
+        self.muons['eta_3jet'+name] = TH1F("muon_eta_3jet"+name,"#eta^{#mu}", 20, -2.1, 2.1)
+        self.muons['eta_4jet'+name] = TH1F("muon_eta_4jet"+name,"#eta^{#mu}", 20, -2.1, 2.1)
+        self.muons['phi'+name] = TH1F("muon_phi"+name,"#phi^{#mu}", 20, 0, 3.15)
+        self.muons['phi_1jet'+name] = TH1F("muon_phi_1jet"+name,"#phi^{#mu}", 20, 0, 3.15)
+        self.muons['phi_2jet'+name] = TH1F("muon_phi_2jet"+name,"#phi^{#mu}", 20, 0, 3.15)
+        self.muons['phi_3jet'+name] = TH1F("muon_phi_3jet"+name,"#phi^{#mu}", 20, 0, 3.15)
+        self.muons['phi_4jet'+name] = TH1F("muon_phi_4jet"+name,"#phi^{#mu}", 20, 0, 3.15)
+        self.muons['reliso'+name] = TH1F("muon_reliso"+name,"Relative Isolation", 30, 0, 1.5)
+        self.muons['reliso_1jet'+name] = TH1F("muon_reliso_1jet"+name,"Relative Isolation", 30, 0, 1.5)
+        self.muons['reliso_2jet'+name] = TH1F("muon_reliso_2jet"+name,"Relative Isolation", 30, 0, 1.5)
+        self.muons['reliso_3jet'+name] = TH1F("muon_reliso_3jet"+name,"Relative Isolation", 30, 0, 1.5)
+        self.muons['reliso_4jet'+name] = TH1F("muon_reliso_4jet"+name,"Relative Isolation", 30, 0, 1.5)
+                
--- Top7TeV/test/tools/skim.py
+++ Top7TeV/test/tools/skim.py
@@ -4,37 +4,46 @@ from ROOT import *
 
 import sys,os, math
 
+import histograms
+
 gROOT.Reset()
 
 gSystem.Load('libYumicevaTop7TeV.so')
 #gROOT.LoadMacro('Yumiceva/Top7TeV/src/TopEventNtupleLinkDef.h+')
 
 # input files
-tfile = TFile("/uscmst1b_scratch/lpc1/cmsroc/yumiceva/tmp/allntuple.root") 
+tfile = TFile("/uscms/home/samvel/Code/ttmuj/NtupleMaker/CMSSW_3_6_3/src/Top/Production/test/prod_25_Aug_2010_1/ntuple_patskim.root")
+#TFile("/uscmst1b_scratch/lpc1/cmsroc/yumiceva/tmp/allntuple.root") 
 tfile.cd()
 
 # get tree
-top = ROOT.gDirectory.Get( 'top' )
+top = ROOT.gDirectory.Get( '/PATNtupleMaker/top' )
 entries = top.GetEntriesFast()
 # loop over entries
 
 evt = TopEventNtuple()
 top.SetBranchAddress('top.', evt)
 cut = {}
-cut['processed'] = cut['OneIsoMuon'] = cut['VetoElectron'] = 0
-cut['1jet'] = cut['2jet'] = cut['3jet'] = cut['4jet'] = 0
+#cut['processed'] = cut['OneIsoMuon'] = cut['VetoElectron'] = 0
+#cut['1jet'] = cut['2jet'] = cut['3jet'] = cut['4jet'] = 0
+cutkeys = ['processed','OneIsoMuon','VetoElectron','1jet','2jet','3jet','4jet']
+for i in cutkeys:
+    cut[i] = 0
 
+hmuon = {}
 hmuon['pt'] = TH1F("muon_pt","p_{T}^{#mu}", 25, 0, 100)
-hmuon['pt_1jet'] = TH1F("muon_pt_1jet","p_{T}^{#mu}", 25,0, 100)
-hmuon['pt_2jet'] = TH1F("muon_pt_2jet","p_{T}^{#mu}", 25,0, 100)
-hmuon['pt_3jet'] = TH1F("muon_pt_3jet","p_{T}^{#mu}", 25,0, 100)
-hmuon['pt_4jet'] = TH1F("muon_pt_4jet","p_{T}^{#mu}", 25,0, 100)
-hmuon['eta'] = TH1F("muon_eta","p_{T}^{#mu}", 25,0, 100)
-hmuon['eta_1jet'] = TH1F("muon_eta_1jet","#eta^{#mu}", 25,0, 100)
-hmuon['eta_2jet'] = TH1F("muon_eta_2jet","#eta^{#mu}", 25,0, 100)
-hmuon['eta_3jet'] = TH1F("muon_eta_3jet","#eta^{#mu}", 25,0, 100)
-hmuon['eta_4jet'] = TH1F("muon_eta_4jet","#eta^{#mu}", 25,0, 100)
+hmuon['pt_1jet'] = TH1F("muon_pt_1jet","p_{T}^{#mu}", 25, 20, 100)
+hmuon['pt_2jet'] = TH1F("muon_pt_2jet","p_{T}^{#mu}", 25, 20, 100)
+hmuon['pt_3jet'] = TH1F("muon_pt_3jet","p_{T}^{#mu}", 25, 20, 100)
+hmuon['pt_4jet'] = TH1F("muon_pt_4jet","p_{T}^{#mu}", 25, 20, 100)
+hmuon['eta'] = TH1F("muon_eta","#eta^{#mu}", 20, -2.1, 2.1)
+hmuon['eta_1jet'] = TH1F("muon_eta_1jet","#eta^{#mu}", 20, -2.1, 2.1)
+hmuon['eta_2jet'] = TH1F("muon_eta_2jet","#eta^{#mu}", 20, -2.1, 2.1)
+hmuon['eta_3jet'] = TH1F("muon_eta_3jet","#eta^{#mu}", 20, -2.1, 2.1)
+hmuon['eta_4jet'] = TH1F("muon_eta_4jet","#eta^{#mu}", 20, -2.1, 2.1)
 
+hM3 = {}
+hM3['3jet'] = TH1F("M3","M3 [GeV/c^{2}]", 20, 0, 500)
 
                    
 for jentry in xrange( entries ):
@@ -71,42 +80,56 @@ for jentry in xrange( entries ):
 
             nmuons += 1
             p4muon.SetPtEtaPhiE( mu.pt, mu.eta, mu.phi, mu.e )
-                        
+            
     if nmuons != 1:
         continue
     cut['OneIsoMuon'] += 1
+    hmuon['pt'].Fill( p4muon.Pt() )
+    hmuon['eta'].Fill( p4muon.Eta() )
     
     for ele in electrons:
         nelec += 1
 
     if nelec > 0 : continue
     cut['VetoElectron'] += 1
+
+    p4jets = []
     
     for jet in jets:
 
         if jet.pt > 30. :
-
+            p4jets.append( TLorentzVector() )
+            p4jets[njets].SetPtEtaPhiE(jet.pt,jet.eta,jet.phi,jet.e)
             njets += 1
-
+            
     if njets > 0:
         cut['1jet'] += 1
     if njets > 1:
         cut['2jet'] += 1
     if njets > 2:
         cut['3jet'] += 1
-    if nejts > 3:
+        p4HadTop = TLorentzVector()
+        p4HadTop = p4jets[0] + p4jets[1] + p4jets[2]
+        hM3['3jet'].Fill( p4HadTop.M() )
+        
+    if njets > 3:
 
         cut['4jet'] += 1
         
-        print "-15 "+str(mupt)+" "+str(mueta)+' '+str(muphi)
-        print "-5 "+str(evt.MET)+" "+str(evt.METeta)+' '+str(evt.METphi)
-        passed += 1
-        for jet in jets:
-
-            print str(jet.e) +' '+str(jet.pt)+' '+str(jet.eta)+' '+str(jet.phi) 
+        #print "-15 "+str(mupt)+" "+str(mueta)+' '+str(muphi)
+        #print "-5 "+str(evt.MET)+" "+str(evt.METeta)+' '+str(evt.METphi)
+        #passed += 1
+        #for jet in jets:
+        #    print str(jet.e) +' '+str(jet.pt)+' '+str(jet.eta)+' '+str(jet.phi) 
 
 print "done."
-print "Cut flow"
-for key in cut.keys():
+print "Cut flow Table"
+for key in cutkeys:
     print key + " " + str(cut[key])
 
+cv1 = TCanvas('muon_pt','muon_pt',600,600)
+hmuon['pt'].Draw()
+cv2 = TCanvas('muon_eta','muon_eta',600,600)
+hmuon['eta'].Draw()
+
+raw_input ("Enter to quit:")
