edb5ff6c35c14f3c12b0ed545a99fbd2cff3e951
yumiceva <>
2010-10-21 16:30:03 +0000

    update

--- cuy/scripts/cuy.py
+++ cuy/scripts/cuy.py
@@ -770,7 +770,7 @@ if __name__ == '__main__':
                             lerrX = []
                             lerrY = []
                             for iibin in range(0,newth.GetNbinsX()):
-                                content = newth.GetBinContent(iibin) + newth.GetBinContent(iibin)*float(thesuper[ikey].Error)
+                                content = newth.GetBinContent(iibin) + newth.GetBinContent(iibin)*float(listError[ii].Error)
                                 lxx.append( newth.GetBinCenter( ibin ) )
                                 lyy.append( newth.GetBinContent(iibin) )
                                 lerrX.append( 0. )
